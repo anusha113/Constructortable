3b855a7c11ebfd3a92605ba4f84433f653391c23
anusha113 <anusha.gangadi.usa@gmail.com>
2021-03-05 22:30:39 -0500

    Initial

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,7 @@
 public class Driver {
     public static void main(String[] args)
-    {
-        //create object
-        Welcome w=new Welcome(10,20);
-        //calling method
-        w.add();
-
+    { //create object
+        Welcome w=new Welcome(10);
     }
 }
 
